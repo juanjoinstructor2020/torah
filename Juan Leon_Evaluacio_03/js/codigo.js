f52e8ee77fc9e3d96d78f63c250391bcab52bb17
juanjoinstructor2020 <67698829+juanjoinstructor2020@users.noreply.github.com>
2020-12-10 07:29:44 -0500

    Add files via upload

---
+++ Juan Leon_Evaluacio_03/js/codigo.js
@@ -0,0 +1,20 @@
+//DESARROLLAR EL CODIGO DE LOS EJERCICIOS  PROPUESTOS MEDIANTE
+//FUNCIONES
+
+//desarrollo 01
+function mostrar(){
+    alert("demostracion de llamar a una funcion");
+}
+
+
+
+
+
+//desarrollo 02
+
+
+
+
+
+
+//desarrollo 03
